95c48ceb089e3bceca0312e382821bb4e16aa8e2
Mateus Gomes <52260710+mateusgomes01@users.noreply.github.com>
2019-10-23 23:17:03 -0300

    Initial commit

---
+++ add.php
@@ -0,0 +1,39 @@
+<?php 
+	//$_GET is a global array that contains all get requests
+	/*
+	if(isset($_GET['submit'])){//if submit has a value it means that data was sent to the server
+		echo $_GET['email'];
+		echo $_GET['title'];
+		echo $_GET['ingredients'];
+	}*/
+
+	if(isset($_POST['submit'])){//if submit has a value it means that data was sent to the server
+		echo htmlspecialchars($_POST['email']);
+		echo htmlspecialchars($_POST['title']);
+		echo htmlspecialchars($_POST['ingredients']);
+	}
+	//<script>window.location = "http://www.thenetninja.co.uk" </script>
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ 	<?php include('templates/header.php') ?>
+
+ 	<section class="container grey-text">
+ 		<h4 class="center">Add a Pizza</h4>
+ 			<form class="white" action="add.php" method="POST">
+ 				<label>Your Email:</label>
+ 				<input type="text" name="email">
+ 				<label>Pizza Title:</label>
+ 				<input type="text" name="title">
+ 				<label>Ingredients (comma separated):</label>
+ 				<input type="text" name="ingredients">
+ 				<div class="center">
+ 					<input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+ 				</div>
+ 			</form>		
+ 	</section>
+
+ 	<?php include('templates/footer.php') ?> 
+
+ </html>
