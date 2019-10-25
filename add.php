4ed1179a56a3a5fa2afbd1aa57e09c902bee2125
Mateus Gomes <52260710+mateusgomes01@users.noreply.github.com>
2019-10-24 22:55:24 -0300

    error messages added
    added error messages bellow input field, and values are kept even after they are submited

--- add.php
+++ add.php
@@ -1,25 +1,47 @@
 <?php 
 	//$_GET is a global array that contains all get requests
 
+	$email = $title = $ingredients = "";
+	$erros = array('email'=>'', 'title'=>'', 'ingredients'=>'');
+
+
 	if(isset($_POST['submit'])){//if submit has a value it means that data was sent to the server
+		
 		//check e-mail
 		if(empty($_POST['email'])){
-			echo 'An e-mail is required <br />';
+			$erros['email'] = 'An e-mail is required <br />';
 		} else {
 			//htmlspecialchars converts special chars to HTML equivalent, to avoid XSS (cross site scripting) attacks
-			echo htmlspecialchars($_POST['email']);
+			//echo htmlspecialchars($_POST['email']);
+			$email = $_POST['email'];
+			//filter_var checks if a value, first argument, is valid based on a filter, second argument
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				$erros['email'] = 'Email must be a valid email address';
+			}
 		}
 
+		//check title
 		if(empty($_POST['title'])){
-			echo 'An title is required <br />';
+			$erros['title'] = 'An title is required <br />';
 		} else {
-			echo htmlspecialchars($_POST['title']);
+			//echo htmlspecialchars($_POST['title']);
+			$title = $_POST['title'];
+			//first parameter is a regex that we want the secont parameter to comply
+			//this regex expression is valid if the variable has characters among regular letters(a-z), capital letters(A-Z) and spaces(\s)
+			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				$erros['title'] = 'Title must be letters and spaces only';
+			}
 		}
 
+		//check ingredients
 		if(empty($_POST['ingredients'])){
-			echo 'At least one ingredient is required <br />';
+			$erros['ingredients'] = 'At least one ingredient is required <br />';
 		} else {
-			echo htmlspecialchars($_POST['ingredients']);
+			//echo htmlspecialchars($_POST['ingredients']);
+			$ingredients = $_POST['ingredients'];
+			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+				$erros['ingredients'] = 'Ingredients must be a comma separated list';
+			}
 		}
 
 
@@ -36,11 +58,14 @@
  		<h4 class="center">Add a Pizza</h4>
  			<form class="white" action="add.php" method="POST">
  				<label>Your Email:</label>
- 				<input type="text" name="email">
+ 				<input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+ 				<div class="red-text"> <?php echo $erros['email']  ?> </div>
  				<label>Pizza Title:</label>
- 				<input type="text" name="title">
+ 				<input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+ 				<div class="red-text"> <?php echo $erros['title']  ?> </div>
  				<label>Ingredients (comma separated):</label>
- 				<input type="text" name="ingredients">
+ 				<input type="text" name="ingredients" value="<?php htmlspecialchars($ingredients) ?>">
+ 				<div class="red-text"> <?php echo $erros['ingredients']  ?> </div>
  				<div class="center">
  					<input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
  				</div>
